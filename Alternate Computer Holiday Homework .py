b36be0a5d2475e12912b531c844bcf4d99ea6dca
Lakshya <104729296+Lakshya-music@users.noreply.github.com>
2022-09-12 15:52:27 +0530

    Update Alternate Computer Holiday Homework .py

--- Alternate Computer Holiday Homework .py
+++ Alternate Computer Holiday Homework .py
@@ -7,11 +7,12 @@ import os
 def Create_rec():
     name_rec = input("Enter the name of the binary record file: ")
     f = open(f"{name_rec}", "ab")
-    n = int(input("How many record do you want to add? "))
+    n = int(input("How many records do you want to add? "))
     for N in range(0, n):
         rollno = int(input("Enter the Roll No.: "))
         name = input("Enter the name: ")
         marks = int(input("Enter the marks: "))
+        print(" ")
         rec = {"Roll No.": rollno, "Name": name, "Marks": marks}
         pickle.dump(rec, f)
     f.close()
@@ -57,37 +58,59 @@ def Search():
 #Modify an existing record in a binary file
 def Modify():
     name_rec = input("Enter the name of the binary record file: ")
-
+    found = False
     rollno_rec_modify = int(input("Enter the Roll No. of the record that you want to modify: "))
   
-    print("1. Modify Name")
-    print("2. Modify Roll No.")
-    print("3. Modify Marks")
-    print("If you want to modify multiple details, enter the required combination of corresponding numbers.")
-    modify = int(input("What do you want to modify? ((1)Name, (2)Roll No., or (3)Marks)"))
+    def modify_rec_details():
+        print("1. Modify Name")
+        print("2. Modify Roll No.")
+        print("3. Modify Marks")
+        print("If you want to modify multiple details, enter the required combination of corresponding numbers.")
+        modify = int(input("What do you want to modify? ((1)Name, (2)Roll No., or (3)Marks)"))
 
-    if modify == 1:
-        new_name = input("Enter the new name: ")
-    elif modify == 2:
-        new_rollno = int(input("Enter the new Roll No.: "))
-    elif modify == 3:
-        new_mark = int(input("Enter the new marks: "))
+        if modify == 1:
+            new_name = input("Enter the new name: ")
+        elif modify == 2:
+            new_rollno = int(input("Enter the new Roll No.: "))
+        elif modify == 3:
+            new_mark = int(input("Enter the new marks: "))
 
-    elif modify == 12 or modify == 21:
-        new_name = input("Enter the new name: ")
-        new_rollno = int(input("Enter the new Roll No.: "))
-    elif modify == 13 or modify == 31:
-        new_name = input("Enter the new name: ")
-        new_mark = int(input("Enter the new marks: "))
-    elif modify == 23 or modify == 32:
-        new_rollno = int(input("Enter the new Roll No.: "))
-        new_mark = int(input("Enter the new marks: "))
-    elif modify == 123 or modify == 132 or modify == 321 or modify == 312 or modify == 213 or modify == 231:
-        new_name = input("Enter the new name: ")
-        new_rollno = int(input("Enter the new Roll No.: "))
-        new_mark = int(input("Enter the new marks: "))
-    else:
-        print("*INVALID COMMAND*")
+        elif modify == 12 or modify == 21:
+            new_name = input("Enter the new name: ")
+            new_rollno = int(input("Enter the new Roll No.: "))
+        elif modify == 13 or modify == 31:
+            new_name = input("Enter the new name: ")
+            new_mark = int(input("Enter the new marks: "))
+        elif modify == 23 or modify == 32:
+            new_rollno = int(input("Enter the new Roll No.: "))
+            new_mark = int(input("Enter the new marks: "))
+        elif modify == 123 or modify == 132 or modify == 321 or modify == 312 or modify == 213 or modify == 231:
+            new_name = input("Enter the new name: ")
+            new_rollno = int(input("Enter the new Roll No.: "))
+            new_mark = int(input("Enter the new marks: "))
+        else:
+            print("*INVALID COMMAND*")
+
+        if modify == 1:
+            record[x]["Name"]  = new_name
+        elif modify == 2:
+            record[x]["Roll No."]  = new_rollno
+        elif modify == 3:
+            record[x]["Marks"]  = new_mark
+            
+        elif modify == 12 or modify == 21:
+            record[x]["Name"]  = new_name
+            record[x]["Roll No."]  = new_rollno
+        elif modify == 13 or modify == 31:
+            record[x]["Name"]  = new_name
+            record[x]["Marks"]  = new_mark
+        elif modify == 23 or modify == 32:
+            record[x]["Roll No."]  = new_rollno
+            record[x]["Marks"]  = new_mark
+        elif modify == 123 or modify == 132 or modify == 321 or modify == 312 or modify == 213 or modify == 231:
+            record[x]["Name"]  = new_name
+            record[x]["Roll No."]  = new_rollno
+            record[x]["Marks"]  = new_mark
 
         
     f = open(f"{name_rec}", "rb")
@@ -102,33 +125,20 @@ def Modify():
 
     for x in range(0, len(record)):
         if record[x]["Roll No."] == rollno_rec_modify:
-
-            if modify == 1:
-                record[x]["Name"]  = new_name
-            elif modify == 2:
-                record[x]["Roll No."]  = new_rollno
-            elif modify == 3:
-                record[x]["Marks"]  = new_mark
+            modify_rec_details()
+            print(" ")
+            print("*Record Modified*")
+            print(" ")
+            found = True
             
-            elif modify == 12 or modify == 21:
-                record[x]["Name"]  = new_name
-                record[x]["Roll No."]  = new_rollno
-            elif modify == 13 or modify == 31:
-                record[x]["Name"]  = new_name
-                record[x]["Marks"]  = new_mark
-            elif modify == 23 or modify == 32:
-                record[x]["Roll No."]  = new_rollno
-                record[x]["Marks"]  = new_mark
-            elif modify == 123 or modify == 132 or modify == 321 or modify == 312 or modify == 213 or modify == 231:
-                record[x]["Name"]  = new_name
-                record[x]["Roll No."]  = new_rollno
-                record[x]["Marks"]  = new_mark
+    if found == True:
+        f = open(f"{name_rec}", "wb")
+        for y in record:
+            pickle.dump(y, f)
+        f.close()
 
-    f = open(f"{name_rec}", "wb")
-    for y in record:
-        pickle.dump(y, f)
-
-    f.close()
+    if found == False: 
+        print("*The record that you want to modify does not exist*")
 
 #Delete an existing record from a binary file
 def Delete():
@@ -151,9 +161,9 @@ def Delete():
             found = True
             continue
         pickle.dump(x, f)
-        if found == False:
-            print("*RECORD NOT FOUND*")
-            break
+    if found == False:
+        print("*RECORD NOT FOUND*")
+        
 
     f.close()
 
@@ -206,7 +216,7 @@ def Insert():
 def delete_binary_file():
     name_rec = input("Enter the name of the record file that you want to Permanently Delete: ")
     print("***")
-    print("Warning: THIS ACTIONS IS GOING TO PERMANENTLY DELETE THE BINARY FILE")
+    print("Warning: THIS ACTION IS GOING TO PERMANENTLY DELETE THE BINARY FILE")
     print("***")
     CHOICE = input("Do you want to procede? (yes/no): ")
     if CHOICE.upper()=="YES":
@@ -219,12 +229,12 @@ def delete_binary_file():
  
 #Purpose
 def Purpose():
-    print("1. Create a binary record/ Append more data to an existing binary record file")
+    print("1. Create a binary record / Append more data to an existing binary record file")
     print("2. Read a binary record file")
     print("3. Search for a record in a binary file")
     print("4. Modify an existing record in a binary file")
     print("5. Delete an existing record from a binary file")
-    print("6. Insert a record between existing records")
+    print("6. Insert a record between existing records in a binary file")
     print("7. Permanently Delete an entire binary file ")
 
     purpose = int(input("What do you want to do? "))
@@ -236,7 +246,6 @@ def Purpose():
         Search()
     elif purpose == 4:
         Modify()
-        print("*RECORD MODIFIED*")
     elif purpose == 5:
         Delete()  
     elif purpose == 6:
